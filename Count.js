c425316ab9185c8aa9d6c6b0b8a9ea562ed96927
shikhasethi1203 <110726410+shikhasethi1203@users.noreply.github.com>
2023-03-01 15:39:41 +0530

    count

--- Count.js
+++ Count.js
@@ -1,15 +1,26 @@
-import React,{useState} from 'react';
-function NameCount(){
-  const[count,setCount]=useState(0);
+import React, { useState } from 'react'
 
-  
-
-return(
-  
-  <div>
-  <p>click me{count}</p>
-<button onClick={()=>(setCount+1)}>
-  click me update</button>
-</div>
-)
+function HookCounterTwo() {
+	const initialCount = 0
+	const [count, setCount] = useState(initialCount)
+	const incrementFive = () => {
+		for (let i = 0; i < 5; i++) {
+			setCount(prevCount => prevCount + 1)
+		}
+	}
+	return (
+		<>
+			Count: {count}
+			<button onClick={() => setCount(initialCount)}>Reset</button>
+			<button onClick={() => setCount(prevCount => prevCount + 1)}>
+				Increment
+			</button>
+			<button onClick={() => setCount(prevCount => prevCount - 1)}>
+				Decrement
+			</button>
+			<button onClick={incrementFive}>Increment 5</button>
+		</>
+	)
 }
+
+export default HookCounterTwo;
